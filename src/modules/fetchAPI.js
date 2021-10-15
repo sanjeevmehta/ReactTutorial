48be916f298851936030b1a0a1cf7d962d27f8d0
Sanjeev Mehta <sanjumeh@gmail.com>
2021-10-15 17:11:42 +0530

    testing

--- src/modules/fetchAPI.js
+++ src/modules/fetchAPI.js
@@ -1,5 +1,6 @@
 export default async function fetchAPI(url) {
-	const response = await fetch(url)
+	const response = await fetch(url, {mode:'cors'});
 	const data = await response.json();
+	console.log({ data });
 	return data;
 }
