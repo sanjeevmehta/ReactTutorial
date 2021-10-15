75885b2f385120a20adcabf317e3871aead73bb7
Sanjeev Mehta <sanjumeh@gmail.com>
2021-10-15 17:20:09 +0530

    API

--- src/modules/fetchAPI.js
+++ src/modules/fetchAPI.js
@@ -1,5 +1,11 @@
 export default async function fetchAPI(url) {
-	const response = await fetch(url, {mode:'cors'});
+	let headers = new Headers();
+	
+	headers.append('Access-Control-Allow-Origin', 'http://localhost:3000');
+	
+	const response = await fetch(url, {'mode':'cors', 'headers': {
+		 'Access-Control-Allow-Origin':'*'
+	}});
 	const data = await response.json();
 	console.log({ data });
 	return data;
