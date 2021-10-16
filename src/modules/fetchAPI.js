f5054e1cd25db10123079da4f805b9e225d0366e
smeh09 <shauryameh09@gmail.com>
2021-10-16 12:25:51 +0530

    fetch test

--- src/modules/fetchAPI.js
+++ src/modules/fetchAPI.js
@@ -1,7 +1,7 @@
 export default async function fetchAPI(url) {
-	const proxy = `https://cors-anywhere.herokuapp.com/`;
 	
-	const response = await fetch(`${proxy}${url}`);
+	
+	const response = await fetch(`${url}`);
 	const data = await response.json();
 	console.log({ data });
 	return data;
